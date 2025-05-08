a9412f2eaa1228eabb200714b6b9849dd138a584
eltahir <eelawad24@wooster.edu>
2025-05-08 16:14:16 -0400

    secretsts

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     // serverComponentsExternalPackages: ['@google-cloud/tasks'], // Old key
+    outputFileTracingIncludes: {
+      '/api/download-chapter/*': [
+        './node_modules/@google-cloud/tasks/build/esm/src/v2/cloud_tasks_client_config.json'
+      ],
+      // If other routes also use @google-cloud/tasks and might face similar issues, 
+      // you could use a broader pattern or add specific entries for them too, e.g.:
+      // '/api/*': ['./node_modules/@google-cloud/tasks/build/esm/src/v2/**/*.json'],
+    },
   },
   serverExternalPackages: ['@google-cloud/tasks'], // New key
   images: {
